7a4670fe8bb920377c181f9623f108163e764e9c
Nazaire02 <nazairendouffou@gmail.com>
2025-01-02 12:05:16 +0000

    banque

---
+++ class/transBanqueItem.tsx
@@ -0,0 +1,10 @@
+export class TransBanqueItem {
+    libelle!: string;
+    tiers?: string;
+    client?: string;
+    source?: string;
+    entree?: string;
+    sortie?: string;
+    solde?: string;
+    date?: string;
+}
